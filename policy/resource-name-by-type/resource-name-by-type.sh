60014f229f7cabf164bdf7f7c89419ccdd19d5c3
Neil Peterson <neilpeterson@outlook.com>
2018-12-09 14:15:50 -0800

    updated policy script

--- policy/resource-name-by-type/resource-name-by-type.sh
+++ policy/resource-name-by-type/resource-name-by-type.sh
@@ -1,5 +1,5 @@
 RESOURCE_GROUP=myPolicyDemo
-POLICY_NAME=aci-name
+POLICY_NAME=myPolicyDemo
 NAME_PATTERN=container##
 RESOURCE_TYPE=Microsoft.ContainerInstance/containerGroups
 POLICY=https://raw.githubusercontent.com/neilpeterson/talk-azure-governance-demo/master/policy/resource-name-by-type/azuredeploy.json
@@ -15,4 +15,4 @@ SCOPE=$(az group show --name $RESOURCE_GROUP --query id -o tsv)
 az policy definition create --name $POLICY_NAME --display-name $POLICY_NAME --description $POLICY_NAME --rules $POLICY --params $PARAM --mode All
 
 # Assign policy
-az policy assignment create --display-name $POLICY_NAME --scope $SCOPE --policy $POLICY_NAME -p '{"namePattern": {"value":"'"$NAME_PATTERN"'"},"resourceType": {"value": "'"$RESOURCE_TYPE"'"'}}'
+az policy assignment create --display-name $POLICY_NAME --scope $SCOPE --policy $POLICY_NAME -p '{"namePattern": {"value":"'"$NAME_PATTERN"'"},"resourceType": {"value": "'"$RESOURCE_TYPE"'"}}'
